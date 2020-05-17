40ed10b0c8cb699b2efc8520a2a322cf06f20b53
Rusmaldi <RianRusmaldi@gmail.com>
2020-05-17 20:42:29 +0700

    Initial Commit

---
+++ Schedule1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Schedule1
+//
+//  Created by Rian Rusmaldi on 17/05/20.
+//  Copyright © 2020 Rian Rusmaldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
